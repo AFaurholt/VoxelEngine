855af4a1d8be7a700b769e62bb1b4af8b1893291
AFaurholt <a_faurholt@outlook.com>
2019-07-15 17:29:18 +0200

    added empty test + added cmake variable passing to the project

--- VoxelEngine/VoxelEngine.cpp
+++ VoxelEngine/VoxelEngine.cpp
@@ -3,6 +3,7 @@
 
 #include "VoxelEngine.h"
 #include "src/AddNum.h"
+#include "ProjectConfig.h"
 #include <string>
 
 using namespace std;
@@ -12,5 +13,10 @@ int main()
 	int tmp = add(1, 4);
 	std::string outStr = "Hello CMake." + std::to_string(tmp);
 	cout << outStr << endl;
+	fprintf(stdout, "Version %d.%d.%d.%d\n",
+		VoxelEngine_VERSION_MAJOR,
+		VoxelEngine_VERSION_MINOR,
+		VoxelEngine_VERSION_PATCH,
+		VoxelEngine_VERSION_TWEAK);
 	return 0;
 }
---
+++ VoxelEngine/test/test_AddNum.cpp
@@ -0,0 +1,2 @@
+#include "../src/AddNum.h"
+#include ""
